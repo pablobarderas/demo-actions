dd12311c98b1298a381a7145155f422693c7356c
pablobaderas <pbaderas31@gmail.com>
2024-04-20 18:01:18 +0200

    Test cicd

---
+++ suma.py
@@ -0,0 +1,5 @@
+def sum (num1, num2):
+    return num1 + num2
+
+if __name__ == "__main__":
+    print(sum(2,2))
---
+++ test_suma.py
@@ -0,0 +1,12 @@
+import unittest
+from suma import sum
+
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(sum(2,2), 4 )
+        self.assertEqual(sum(2,-2), 0 )
+        self.assertEqual(sum(2,-3), -1 )
+
+if __name__ == "__main__":
+    unittest.main()
